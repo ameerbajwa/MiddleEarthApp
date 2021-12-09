198a9316e0036b501898e5ec034b6a291fe38345
Ameer Bajwa <ameerbajwa@gmail.com>
2021-12-08 16:43:16 -0800

    Created new project to learn MVP and MVVM architecture while using coordinators

---
+++ MiddleEarthApp/MiddleEarthApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiddleEarthApp
+//
+//  Created by Ameer Bajwa on 12/8/21.
+//  Copyright © 2021 Ameer Bajwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
